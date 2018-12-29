1b4abd53a69514241136700f59e17437c2ccd97f
Alex Posysoev <alekseyposysoev@gmail.com>
2018-12-29 18:12:01 +0300

    test

--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -13,20 +13,11 @@ export class TestService {
   public units = [];
   public details = [];
   public originalNumber = [];
-  constructor(private _apiServise: ApiService) { }
-
-  test(): Observable<any> {
-    return this._apiServise.getCars().pipe(map(data => {
+  constructor(private _apiServise: ApiService) {
+    this._apiServise.getCars().subscribe(data => {
       this.markName = data;
-    }));
-  }
+    });
 
-  test1(id, da): Observable<any> {
-    this.test();
-    return this._apiServise.getModelName(id).pipe(map(data => {
-      da = data;
-      console.log(da);
-    }));
   }
 
   // test3(id, id2): Observable<any> {
